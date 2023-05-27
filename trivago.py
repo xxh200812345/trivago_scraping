2d12024d52c892eb1dd66fecaf6c711d0a9d5890
xxh200812345 <46435236+xxh200812345@users.noreply.github.com>
2023-05-28 01:32:08 +0900

    2.1.0

--- trivago.py
+++ trivago.py
@@ -216,11 +216,17 @@ def inittilt(nowsheet):
 
 
 def scraping(
-    search_cityname, search_checkin, search_checkout, real_currency, search_star
+    search_cityname,
+    search_checkin,
+    search_checkout,
+    real_currency,
+    search_star,
+    accommodation_list,
 ):
-    global i
+    """
+    读取当前页的住宿清单
+    """
     global nowsheet
-    n = 1
 
     # 将滚动条移动到页面的最底部
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -229,93 +235,90 @@ def scraping(
     time.sleep(2)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+    i = nowsheet_i
+
+    for page in accommodation_list:
+        for item in page:
+            if item:
+                i += 1
+
     s = BeautifulSoup(driver.page_source, "html.parser")
     hotelitems = s.find_all("li", attrs={"data-testid": "accommodation-list-element"})
 
+    n = 1
+    page = []
+    accommodation_list.append(page)
+
     for hotelitem in hotelitems:
-        try:
-            """
-            星级信息筛选
-            """
-            stardiv = hotelitem.find("span", attrs={"itemprop": "starRating"})
-            if stardiv != None:
-                stardiv = stardiv.find("meta")["content"]
-            else:
-                stardiv = "0"
-            if stardiv not in search_star:
-                continue
+        out_put = {}
+        page.append(out_put)
 
-            i += 1
-            nowsheet["A" + str(i)] = str(row_num - 1) + "-" + str(n)
-
-            nowsheet["B" + str(i)] = search_cityname
-
-            hotelname = hotelitem.find("span", attrs={"itemprop": "name"}).text
-            nowsheet["C" + str(i)] = hotelname
-
-            clickout_area = hotelitem.find(
-                "div", attrs={"data-testid": "clickout-area"}
-            )
-            bookingsite = clickout_area.find(
-                "strong", attrs={"itemprop": "offeredBy"}
-            ).text
-            nowsheet["D" + str(i)] = bookingsite
-
-            hotelprice = clickout_area.find("p", attrs={"itemprop": "price"}).text
-            hotelprice
-            try:
-                nowsheet["E" + str(i)] = int(hotelprice.lstrip("$").replace(",", ""))
-            except:
-                nowsheet["E" + str(i)] = hotelprice.lstrip("$").replace(",", "")
-
-            star = hotelitem.find("meta")["content"]
-            nowsheet["F" + str(i)] = star
-
-            alternative_deal = hotelitem.find(
-                "button", attrs={"data-testid": "alternative-deal"}
-            )
-            if alternative_deal != None:
-                bookingsite = alternative_deal.find(
-                    "span", attrs={"itemprop": "name"}
-                ).text
-                nowsheet["G" + str(i)] = bookingsite
-
-                hotelprice = alternative_deal.find(
-                    "span", attrs={"class", "text-l"}
-                ).text
-                try:
-                    nowsheet["H" + str(i)] = int(
-                        hotelprice.lstrip("$").replace(",", "")
-                    )
-                except:
-                    nowsheet["H" + str(i)] = hotelprice.lstrip("$").replace(",", "")
-
-            more_deals = hotelitem.find("button", attrs={"data-testid": "more-deals"})
-            if more_deals != None:
-                bookingsite = more_deals.find("span", attrs={"itemprop": "name"}).text
-                nowsheet["I" + str(i)] = bookingsite
-
-                hotelprice = more_deals.find("span", attrs={"class", "text-l"}).text
-                try:
-                    nowsheet["J" + str(i)] = int(
-                        hotelprice.lstrip("$").replace(",", "")
-                    )
-                except:
-                    nowsheet["J" + str(i)] = hotelprice.lstrip("$").replace(",", "")
-
-            nowsheet["M" + str(i)] = search_checkin.strftime("%Y-%m-%d")
-            nowsheet["N" + str(i)] = search_checkout.strftime("%Y-%m-%d")
-
-            nowsheet["O" + str(i)] = real_currency
-
-            n = n + 1
-        except:
-            i = i - 1
+        """
+        星级信息筛选
+        """
+        stardiv = hotelitem.find("span", attrs={"itemprop": "starRating"})
+        if stardiv != None:
+            stardiv = stardiv.find("meta")["content"]
+        else:
+            stardiv = "0"
+        if stardiv not in search_star:
             continue
 
-    logger.info(
-        f"## 读取到{len(hotelitems)}个房源，实际输出到文件{n-1}个"
-    )
+        i += 1
+
+        out_put["A" + str(i)] = (
+            str(row_num - 1) + "-" + str(len(accommodation_list)) + "-" + str(n)
+        )
+
+        out_put["B" + str(i)] = search_cityname
+
+        hotelname = hotelitem.find("span", attrs={"itemprop": "name"}).text
+        out_put["C" + str(i)] = hotelname
+
+        clickout_area = hotelitem.find("div", attrs={"data-testid": "clickout-area"})
+        bookingsite = clickout_area.find("strong", attrs={"itemprop": "offeredBy"}).text
+        out_put["D" + str(i)] = bookingsite
+
+        hotelprice = clickout_area.find("p", attrs={"itemprop": "price"}).text
+        hotelprice
+        try:
+            out_put["E" + str(i)] = int(hotelprice.lstrip("$").replace(",", ""))
+        except:
+            out_put["E" + str(i)] = hotelprice.lstrip("$").replace(",", "")
+
+        star = hotelitem.find("meta")["content"]
+        out_put["F" + str(i)] = star
+
+        alternative_deal = hotelitem.find(
+            "button", attrs={"data-testid": "alternative-deal"}
+        )
+        if alternative_deal != None:
+            bookingsite = alternative_deal.find("span", attrs={"itemprop": "name"}).text
+            out_put["G" + str(i)] = bookingsite
+
+            hotelprice = alternative_deal.find("span", attrs={"class", "text-l"}).text
+            try:
+                out_put["H" + str(i)] = int(hotelprice.lstrip("$").replace(",", ""))
+            except:
+                out_put["H" + str(i)] = hotelprice.lstrip("$").replace(",", "")
+
+        more_deals = hotelitem.find("button", attrs={"data-testid": "more-deals"})
+        if more_deals != None:
+            bookingsite = more_deals.find("span", attrs={"itemprop": "name"}).text
+            out_put["I" + str(i)] = bookingsite
+
+            hotelprice = more_deals.find("span", attrs={"class", "text-l"}).text
+            try:
+                out_put["J" + str(i)] = int(hotelprice.lstrip("$").replace(",", ""))
+            except:
+                out_put["J" + str(i)] = hotelprice.lstrip("$").replace(",", "")
+
+        out_put["M" + str(i)] = search_checkin.strftime("%Y-%m-%d")
+        out_put["N" + str(i)] = search_checkout.strftime("%Y-%m-%d")
+
+        out_put["O" + str(i)] = real_currency
+
+        n = n + 1
 
 
 def inputcityname(cityname):
@@ -339,6 +342,9 @@ def inputcityname(cityname):
 
 
 def inputcurrency(search_currency):
+    """
+    变更货币类型
+    """
     driver.delete_all_cookies()
     # Due to the internet speed, the currency selection will be loaded incorrectly. If the currency selection is wrong, please increase the sleep time here.
     # For example, change to time.sleep(2)
@@ -384,8 +390,28 @@ def logging_init():
     print("selflogger", my_module.handlers)
     # print("子记录器与根记录器的handler是否相同：", root.handlers[0] == my_module.handlers[0])
     my_module.error("DUBUG")
-    logger.info(""*3)
+    logger.info("" * 3)
     logger.info("-----------START")
+
+
+def data_output_to_excel(accommodation_list):
+    """
+    住宿清单写入EXCEL
+    """
+    global nowsheet_i
+    global nowsheet
+
+    for i, page in enumerate(accommodation_list):
+        # 初始化计数器
+        count = 0
+        for hotelitem in page:
+            if hotelitem:
+                count += 1
+                for key, value in hotelitem.items():
+                    nowsheet[key] = value
+        nowsheet_i += count
+
+        logger.info(f"## 第{(i+1)}页读取到{len(page)}个房源，实际输出到文件{count}个")
 
 
 def body(search_sheet, nowsheet):
@@ -441,10 +467,20 @@ def body(search_sheet, nowsheet):
     bot_check_wait(3)
     driver.get(url)
 
+    # 住宿清单
+    accommodation_list = []
+
+    # 第一页
     scraping(
-        search_cityname, search_checkin, search_checkout, search_currency, search_star
+        search_cityname,
+        search_checkin,
+        search_checkout,
+        search_currency,
+        search_star,
+        accommodation_list,
     )
 
+    # 每一页
     while check_next_btn():
         click_next_btn()
 
@@ -454,7 +490,11 @@ def body(search_sheet, nowsheet):
             search_checkout,
             search_currency,
             search_star,
+            accommodation_list,
         )
+
+    # 住宿清单写入EXCEL
+    data_output_to_excel(accommodation_list)
 
     return True
 
@@ -529,13 +569,14 @@ def open_file_dialog():
 
     return file_path
 
+
 if __name__ == "__main__":  # ⼊⼝函数
     logging_init()
 
     config_init()
-    file_path=open_file_dialog()
+    file_path = open_file_dialog()
 
-    logger.info(config.get("Section","version"))
+    logger.info(config.get("Section", "version"))
 
     # 创建文件夹
     result_dir = "output"
@@ -553,7 +594,7 @@ if __name__ == "__main__":  # ⼊⼝函数
     # 保存结果的excel生成
     retexl = px.Workbook()
     nowsheet = retexl.active
-    i = 1
+    nowsheet_i = 1
 
     logger.info(f"# 调查任务开始")
 
